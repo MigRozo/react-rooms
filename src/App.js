23836f4d683d54200e6da07555fbaa76da56d28f
Mig <angelorozo13@hotmail.com>
2022-05-04 22:44:54 -0500

    react app created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="rooms">
+      <div className="roomsNav">
+        <button className="roomsNav_item" data-tag="all">all</button>
+        <button className="roomsNav_item" data-tag="resorts">Resorts</button>
+        <button className="roomsNav_item" data-tag="villas">Villas</button>
+      </div>
+      <div className="roomsList">
+        <article className="room">room1</article>
+        <article className="room" data-tag="resorts">room2</article>
+        <article className="room" data-tag="villas">room3</article>
+      </div>
     </div>
   );
 }
